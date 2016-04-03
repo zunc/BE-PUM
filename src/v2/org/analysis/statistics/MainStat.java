970203a3a5143e1b794d83ba9c15630fe7b7958c
timata <551307910@hcmut.edu.vn>
2016-04-03 23:56:19 +0700

    Insert the method for analyzing Log file.

---
+++ src/v2/org/analysis/statistics/MainStat.java
@@ -0,0 +1,65 @@
+package v2.org.analysis.statistics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class MainStat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Begin to analyze log file!");
+		logAnalysis("asm/log/", "data/data/multiResult.txt");
+		System.out.println("Finish!");
+	}
+	
+	public static void logAnalysis(String myDirectoryPath, String result) {		
+		 File dir = new File(myDirectoryPath);
+		  File[] directoryListing = dir.listFiles();
+		  FileProcess ret = new FileProcess(result);
+		  FileProcess interrupt = new FileProcess("data/data/interruptResult.txt");
+		  		  
+		  if (directoryListing != null) {
+		    for (File child : directoryListing) {
+		      // Do something with child
+		    	try {
+					// clearContentFile(unprocessedFile);
+					BufferedReader br = new BufferedReader(new FileReader(child.getAbsolutePath()));
+					// StringBuilder sb = new StringBuilder();
+					String line = br.readLine();
+					int num = 0;
+					String fileName = child.getName().substring(6);
+					fileName = fileName.replace(".log", "");
+					while (line != null) {
+												
+						if (line.contains("START NEW OTF THREAD")) {
+							num ++; 
+							// Main.process("-m " + pathVirus + line);
+							if (num == 2) {								
+								System.out.println("MultiThread:" + fileName);
+								ret.appendFile(fileName);							
+							}
+						}
+						if (line.contains("INTERRUPT OTF THREAD")) {
+							System.out.println("Interrupt:" + fileName);
+							interrupt.appendFile(fileName);
+						}
+						line = br.readLine();
+						// if (numFile > 0) break;
+					}
+					br.close();
+				} catch (Exception e) {
+					System.out.println(e.toString());
+				}
+
+		    	
+		    }
+		  } else {
+		    // Handle the case where dir is not really a directory.
+		    // Checking dir.isDirectory() above would not be sufficient
+		    // to avoid race conditions with another process that deletes
+		    // directories.
+		  }
+	}
+
+}
