44ffdd8e1e9f8561f1f258e3739ad21920f883e7
timata <551307910@hcmut.edu.vn>
2016-04-06 23:15:45 +0700

    Insert method for analyzing frequency file.

--- src/v2/org/analysis/statistics/MainStat.java
+++ src/v2/org/analysis/statistics/MainStat.java
@@ -8,21 +8,21 @@ public class MainStat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Begin to analyze log file!");
-		logAnalysis("asm/log/", "data/data/multiResult.txt");
+		System.out.println("Begin to analyze Frequency file!");
+		logFrequency("asm/log/freq.log", "data/data/freqResult.txt");
 		System.out.println("Finish!");
 	}
-	
-	public static void logAnalysis(String myDirectoryPath, String result) {		
-		 File dir = new File(myDirectoryPath);
-		  File[] directoryListing = dir.listFiles();
-		  FileProcess ret = new FileProcess(result);
-		  FileProcess interrupt = new FileProcess("data/data/interruptResult.txt");
-		  		  
-		  if (directoryListing != null) {
-		    for (File child : directoryListing) {
-		      // Do something with child
-		    	try {
+
+	public static void logAnalysis(String myDirectoryPath, String result) {
+		File dir = new File(myDirectoryPath);
+		File[] directoryListing = dir.listFiles();
+		FileProcess ret = new FileProcess(result);
+		FileProcess interrupt = new FileProcess("data/data/interruptResult.txt");
+
+		if (directoryListing != null) {
+			for (File child : directoryListing) {
+				// Do something with child
+				try {
 					// clearContentFile(unprocessedFile);
 					BufferedReader br = new BufferedReader(new FileReader(child.getAbsolutePath()));
 					// StringBuilder sb = new StringBuilder();
@@ -31,13 +31,13 @@ public class MainStat {
 					String fileName = child.getName().substring(6);
 					fileName = fileName.replace(".log", "");
 					while (line != null) {
-												
+
 						if (line.contains("START NEW OTF THREAD")) {
-							num ++; 
+							num++;
 							// Main.process("-m " + pathVirus + line);
-							if (num == 2) {								
+							if (num == 2) {
 								System.out.println("MultiThread:" + fileName);
-								ret.appendFile(fileName);							
+								ret.appendFile(fileName);
 							}
 						}
 						if (line.contains("INTERRUPT OTF THREAD")) {
@@ -52,14 +52,53 @@ public class MainStat {
 					System.out.println(e.toString());
 				}
 
-		    	
-		    }
-		  } else {
-		    // Handle the case where dir is not really a directory.
-		    // Checking dir.isDirectory() above would not be sufficient
-		    // to avoid race conditions with another process that deletes
-		    // directories.
-		  }
+			}
+		} else {
+			// Handle the case where dir is not really a directory.
+			// Checking dir.isDirectory() above would not be sufficient
+			// to avoid race conditions with another process that deletes
+			// directories.
+		}
+	}
+
+	public static void logFrequency(String myPath, String result) {
+		FileProcess retsultFile = new FileProcess(result);
+
+		try {
+			// clearContentFile(unprocessedFile);
+			BufferedReader br = new BufferedReader(new FileReader(myPath));
+			// StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				int[] freq = new int[14];
+				for (int i = 0; i < freq.length; i++) {
+					freq[i] = 0;
+				}
+
+				String[] freqString = line.split("_");
+				for (int i = 0; i < freqString.length; i++) {
+					if (freqString[i] != null && freqString[i] != "") {
+						try {
+							freq[Integer.parseInt(freqString[i]) - 1]++;
+						} catch (Exception e) {
+							System.out.println(e.toString());
+						}
+					}
+				}
+				String resultFreq = "";
+				for (int i = 0; i < freq.length; i++) {
+					resultFreq += freq[i] + "\t";
+				}
+
+				retsultFile.appendFile(resultFreq);
+				line = br.readLine();
+				// if (numFile > 0) break;
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
 	}
 
 }
